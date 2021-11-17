1f5da46fe6dabb3ac999c64f96b9211c3cfd9da1
Matthew Dowell <mdowell723@gmail.com>
2021-11-17 15:16:49 -0700

    setup schema

---
+++ schema.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+\c reviews;
+
+CREATE TABLE characteristics (
+  id SERIAL PRIMARY KEY,
+  product_id INT,
+  name VARCHAR(100)
+);
+
+COPY characteristics FROM '/Users/matthewdowell/Documents/Immersive/reviewsAPI/data/characteristics.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE characteristic_reviews (
+  id SERIAL PRIMARY KEY,
+  characteristic_id INT,
+  review_id INT,
+  value INT
+);
+
+COPY characteristic_reviews FROM '/Users/matthewdowell/Documents/Immersive/reviewsAPI/data/characteristic_reviews.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE reviews_photos (
+  id SERIAL PRIMARY KEY,
+  review_id INT,
+  url VARCHAR(500)
+);
+
+COPY reviews_photos FROM '/Users/matthewdowell/Documents/Immersive/reviewsAPI/data/reviews_photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  product_id INT,
+  rating INT,
+  date BIGINT,
+  summary VARCHAR(300),
+  body VARCHAR(1000),
+  recommend BOOLEAN,
+  reported BOOLEAN,
+  reviewer_name VARCHAR(100),
+  reviewer_email VARCHAR(100),
+  response VARCHAR(500),
+  helpfulness INT
+);
+
+COPY reviews FROM '/Users/matthewdowell/Documents/Immersive/reviewsAPI/data/reviews.csv'
+DELIMITER ','
+CSV HEADER;
